35e9fe2438e9085346ddc1b03ef08d2e34a5c419
Jeremiah <esiebomaj@gmail.com>
2023-12-21 12:44:07 +0100

    added host on app run

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ app.add_url_rule('/graphql', view_func=schema.graphql_view,
                  methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
